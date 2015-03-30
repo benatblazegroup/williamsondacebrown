25acaa82581bf982afb18d496914a371fbaef4bc
Ben Read <endymion1818@gmail.com>
2015-03-30 10:09:46 +0100

    header edits
     Reordered navbar toggle; added text to button, other CSS changes to
    navbar.

--- siteadmin/templates/layouts/blog.header.php
+++ siteadmin/templates/layouts/blog.header.php
@@ -21,10 +21,7 @@
     <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+          <span>Toggle navigation</span>
         </button>
         <a class="navbar-brand" href="../index.php"><img src="/assets/img/wdb-logo.svg"></a>
       </div>
--- siteadmin/templates/layouts/global.header.php
+++ siteadmin/templates/layouts/global.header.php
@@ -20,13 +20,10 @@
   <nav class="navbar">
     <div class="container">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="navbar-brand" href="index.php"><img src="/assets/img/wdb-logo.svg"></a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span>Toggle navigation&nbsp;<i class="fa fa-bars"></i></span>
+        </button>
       </div>
       <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
