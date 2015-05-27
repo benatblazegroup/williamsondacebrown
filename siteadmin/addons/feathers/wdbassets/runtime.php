e914c225a3f08bc30a7e57cb83fd433b6f12dc83
Ben Read <ben@blazegroup.co.uk>
2015-05-27 09:56:49 +0100

    Replaced Siteadmin folder from live

--- siteadmin/addons/feathers/wdbassets/runtime.php
+++ siteadmin/addons/feathers/wdbassets/runtime.php
@@ -24,7 +24,7 @@ class PerchFeather_wdbassets extends PerchFeather
 
         if (!$this->component_registered('jquery')) {
     			$out[] = $this->_script_tag(array(
-    				'src'=>'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'
+    				'src'=>'/assets/js/jquery-1.11.2.min.js'
     			));
     			$this->register_component('jquery');
     		};
