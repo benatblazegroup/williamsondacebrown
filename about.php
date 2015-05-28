5ee3471588b691c077262754e2b700dbb695af88
Ben Read <ben@blazegroup.co.uk>
2015-05-28 13:12:17 +0100

    Changes to About page

--- about.php
+++ about.php
@@ -19,37 +19,154 @@
       <p>From our office in Southgate, North London, we have been looking after client’s property building up long term relationships with both landlords and tenants for the past fifty years. </p>
       <p>Both partners of WDB are qualified chartered surveyors and the firm is therefore regulated by the RICS (Royal Institution of Chartered Surveyors). The RICS are the world's leading professional body for qualifications and standards in land, property and construction. We are also regulated by the Tenancy Deposit Scheme and the Financial Conduct Authority.  </p>
       <hr>
-      <h1>Our Team</h1>
+      <h2>Partners / Chartered Surveyors</h2>
       <div class="team-block row">
-        <div class="col-sm-3">
+        <div class="col-sm-3 col-sm-offset-3">
           <div class="team-feature">
-            <img class="img-circle" src="/assets/img/dace.png">
-            <p><strong>Katherine Dace RD BSc FRICS</strong></p>
-            <p>Katherine Dace: Qualified in 1984, worked in central London becoming a partner in W. Goodchild &amp; company in 1987 before merging her practise with Peter Williamson in 1996. Specialising in property management and leasehold reform act valuations. </p>
+            <img class="img-circle" src="/assets/img/kate-dace.png">
+            <p><strong>Kate Dace</strong></p>
+            <p>Partner, Chartered Surveyor<br/>
+               020 886 4407</p>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="team-feature">
-            <img class="img-circle" src="/assets/img/brown.png">
+            <img class="img-circle" src="/assets/img/alex-brown.png">
             <br>
-            <p><strong>Alexander Brown BSC (Hons) MRICS</strong></p>
-            <p>Alexander Brown: Qualified in 2006 as a rural surveyor in East Anglia. Alex moved to Williamson &amp; Dace in 2007 being promoted to partner in 2012. Specialising in property management and landlord and tenant issues. </p>
+            <p><strong>Alex Brown</strong></p>
+            <p>Partner, Chartered Surveyor<br/>
+               020 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Graduate Surveyors</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/tom-dace.png">
+            <p><strong>Thomas Dace</strong></p>
+            <p>Graduate Surveyor<br/>
+               020 8920 2496</p>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="team-feature">
-            <img class="img-circle" src="/assets/img/placeholder-female.png">
+            <img class="img-circle" src="/assets/img/zeki-ansal.png">
             <br>
-            <p><strong>Placeholder</strong></p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut orci id ante adipiscing tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+            <p><strong>Zeki Ansal</strong></p>
+            <p>Graduate Building Surveyor<br/>
+               020 8920 2497</p>
+          </div>
+        </div>
+      </div>
+      <h2>Residential Lettings</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-point5">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/jacky-parnell.png">
+            <p><strong>Jacqueline Parnell</strong></p>
+            <p>Senior Lettings Negotiator<br/>
+               020 8920 2494</p>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="team-feature">
-            <img class="img-circle" src="/assets/img/placeholder-male.png">
+            <img class="img-circle" src="/assets/img/kristina-varnava.png">
             <br>
-            <p><strong>Placeholder</strong></p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut orci id ante adipiscing tempor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p><strong>Kristina Varnava</strong></p>
+            <p>Lettings Negotiator<br/>
+               020 8920 2499</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-feature">
+            <img class="img-circle img-background" src="/assets/img/nopicture.png">
+            <br>
+            <p><strong>Janet Reardon</strong></p>
+            <p>Lettings Negotiator<br/>
+               0208 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Service charge/Building Insurance</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/sophia-orphanides.png">
+            <p><strong>Sophia Orphanides</strong><br/>
+            0208 886 4407</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/karen-rider.png">
+            <br>
+            <p><strong>Karen Rider</strong><br/>
+            0208 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Accounts</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-point5">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/alan-dewberry.png">
+            <p><strong>Alan Dewberry</strong></p>
+            <p>0208 886 4407</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/sheila-wilson.png">
+            <br>
+            <p><strong>Sheila Wilson</strong></p>
+            <p>0208 886 4407</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-feature">
+            <img class="img-circle img-background" src="/assets/img/nopicture.png">
+            <br>
+            <p><strong>Alison Bryant</strong></p>
+            <p>0208 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Secretary</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-4point5">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/pauline-fellerman.png">
+            <p><strong>Pauline Fellerman</strong><br/>
+            0208 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Reception / Administration</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/thelia-farago.png">
+            <p><strong>Thelia Farago</strong><br/>
+               020 886 4407</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/lida-litras.png">
+            <br>
+            <p><strong>Lida Litras</strong><br/>
+               020 886 4407</p>
+          </div>
+        </div>
+      </div>
+      <h2>Maintenance</h2>
+      <div class="team-block row">
+        <div class="col-sm-3 col-sm-offset-4point5">
+          <div class="team-feature">
+            <img class="img-circle" src="/assets/img/michael-coyne.png">
+            <p><strong>Michael Coyne</strong><br/>
+            0208 886 4407</p>
           </div>
         </div>
       </div>
